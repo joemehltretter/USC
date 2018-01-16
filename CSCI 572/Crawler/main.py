bc47909ba22c0ea86ebc3a5b5a21dea2551aee76
joemehltretter <31524161+joemehltretter@users.noreply.github.com>
2018-01-16 14:36:44 -0800

    Add relevancy functionality
    Add functionality to analyze text for keywords.

--- CSCI 572/Crawler/main.py
+++ CSCI 572/Crawler/main.py
@@ -2,28 +2,52 @@ from bs4 import BeautifulSoup as bs
 import requests
 import queue
 import urllib.request
+import string
 
 def main():
   #Declare seeds
   seedUrls = ['https://threatpost.com/blog', 'https://hackread.com/',\
   'http://ehackingnews.com/', 'https://us-cert.gov/ncas/current-activity']
+  relevantArticles = {}
+  keywords = []
+  keywordPath = "/Users/jmehl/Documents/Github/USC/CSCI 572/Crawler/words.txt"
+  file = open(keywordPath, 'r')
+  for word in file:
+    keywords.append(word)
+
   linkQueue = queue.Queue()
   session = requests.session()
   for seed in seedUrls:
     url = ''.join(seed)
     req = urllib.request.Request(url)
     req.add_header('User-Agent', 'Mozilla/5.0')
-    req.encoding = "utf-8"
-    urlPage = urllib.request.urlopen(req).read()
-    soupObject = bs(urlPage, "lxml")
-    pageLinks = soupObject.find_all('a')
-    print("Printing for {url}".format(url = url))
-    if pageLinks:
-      for link in pageLinks:
-        if link.has_attr('href'):
-          #print(link['href'].encode("utf-8"))
-          #Analyze link if relevant store in queue
-          continue
-    
+    urlPage = urllib.request.urlopen(req)
+    charset = urlPage.info().get_content_charset()
+    print(charset)
+    pageContent = urlPage.read().decode(charset)
+    soupObject = bs(pageContent, "html.parser")
+    #pageLinks = soupObject.find_all('a')
+    for pageLinks in soupObject.find_all('a'):
+      #print(type(pageLinks))
+      if pageLinks:
+        #for link in pageLinks:
+        if pageLinks.has_attr('href'):
+          articleTitle = pageLinks.text.strip()
+          print(articleTitle.encode('utf-8', 'ignore').decode('utf-8'))
+          articleUrl = pageLinks['href']
+          relevant = False
+          for word in keywords:
+            if word in articleTitle:
+              relevant = True
+              print("True")
+              #relevantArticles[articleTitle] = articleUrl
+              print(word)
+              break
+            
+            else:
+              continue
+            #print(articleTitle.encode("utf-8"))
+    for url in relevantArticles:
+      print("{url} : {title}".format(url = url, title = relevantArticles['url']))
 if __name__ == "__main__":
     main()
