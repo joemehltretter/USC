0344f2faa7de0c5c5188fc6ca6adafe4908a173f
joemehltretter <mehltret@usc.edu>
2018-01-17 21:56:53 -0800

    Add file name creation, body text extraction, and save body

--- CSCI 572/Crawler/main.py
+++ CSCI 572/Crawler/main.py
@@ -1,15 +1,19 @@
 from bs4 import BeautifulSoup as bs
 import requests
-import queue
+from collections import deque
 import urllib.request
+
+## Global Variables ##
+linkQueue = deque()
+path = '/home/mehltret/Desktop/USC/CSCI 572/Crawler/files/'
+##
 
 def crawl(url):
   url = ''.join(url)
   req = urllib.request.Request(url)
   req.add_header('User-Agent', 'Mozilla/5.0')
   urlPage = urllib.request.urlopen(req)
-  charset = urlPage.info().get_content_charset()
-  pageContent = urlPage.read().decode(charset)
+  pageContent = urlPage.read()
   soupObject = bs(pageContent, "html.parser")
   allLinks = soupObject.find_all('a')
   return(allLinks)
@@ -21,26 +25,53 @@ def extract(extractUrls):
       if pageLinks.has_attr('href'):
         articleTitle = pageLinks.text.strip().lower()
         articleUrl = pageLinks['href']
-        articles[articleTitle] = articleUrl
+        if 'http' in articleUrl:
+          articles[articleTitle] = articleUrl
   return(articles)
 
 def relevance_check(articles, keywords):
   rel = {}
   for key in articles:
-    #relevant = False
     for word in keywords:
       if word in key and len(key.split()) > 3:
-        rel[key] = articles[key]
+        url = articles[key]
+        rel[key] = url
+        get_body_and_save(key, url)
+        linkQueue.append(articles[key])
+        print("# of relevant articles found: {num}".format(num = len(linkQueue)))
         break
 
       else:
         continue
+
   return(rel)
+
+def get_body_and_save(title, url):
+  title = title.replace(',', '')
+  title = title.replace('.', '')
+  title = title.replace("'", "")
+  title = title.replace(' ', '_')
+  
+  req = urllib.request.Request(url)
+  req.add_header('User-Agent', 'Mozilla/5.0')
+  urlPage = urllib.request.urlopen(req)
+  pageContent = urlPage.read()
+  soupObject = bs(pageContent, "html.parser")
+  bodyText = soupObject.find_all('p')
+
+  body = []
+  for p in bodyText:
+    body.append(p.text)
+  
+  body = ''.join(body)
+  fullPath = path + title
+  with open(fullPath, 'w') as newFile:
+    newFile.write(body)
 
 def main():
   #Declare seeds
-  seedUrls = ['https://threatpost.com/blog', 'https://hackread.com/',\
-  'http://ehackingnews.com/', 'https://us-cert.gov/ncas/current-activity']
+  seedUrls = ['https://threatpost.com', 'https://hackread.com',\
+  'http://ehackingnews.com', 'https://us-cert.gov/ncas/current-activity']
   articles = {}
   relevantArticles = {}
   keywords = []
@@ -49,15 +80,23 @@ def main():
   for word in file:
     keywords.append(word.lower().strip())
 
-  linkQueue = queue.Queue()
-  session = requests.session()
   for seed in seedUrls:
-    allLinks = crawl(seed)
-    articles = extract(allLinks)
-    relevantArticles = relevance_check(articles, keywords)
+    foundLinks = crawl(seed)
+    articles = extract(foundLinks)
+    relevantArticles.update(relevance_check(articles, keywords))
 
-    for key in relevantArticles:
-      print("{title} + {url}".format(title = key, url = relevantArticles[key]))
+  while len(linkQueue) > 0:
+    print("Queue current length is: {size} \n".format(size = len(linkQueue)))
+    url = linkQueue.popleft()
+    foundLinks = crawl(url)
+    articles = extract(foundLinks)
+    relevantArticles.update(relevance_check(articles, keywords))
+    print("Current length of relevant article dic : {size}".format(size = len(relevantArticles)))
+#    if len(linkQueue) > 1000:
+#      break
+
+  for key in relevantArticles:
+    print("{title} + {url}".format(title = key, url = relevantArticles[key]))
 
 if __name__ == "__main__":
-    main()
+  main()
