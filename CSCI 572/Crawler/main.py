0ef50edb082d5d72e2d4fcdfe5b4cf09f6c1e9b0
joemehltretter <mehltret@usc.edu>
2018-01-17 17:49:57 -0800

    refractor

--- CSCI 572/Crawler/main.py
+++ CSCI 572/Crawler/main.py
@@ -2,7 +2,40 @@ from bs4 import BeautifulSoup as bs
 import requests
 import queue
 import urllib.request
-#import string
+
+def crawl(url):
+  url = ''.join(url)
+  req = urllib.request.Request(url)
+  req.add_header('User-Agent', 'Mozilla/5.0')
+  urlPage = urllib.request.urlopen(req)
+  charset = urlPage.info().get_content_charset()
+  pageContent = urlPage.read().decode(charset)
+  soupObject = bs(pageContent, "html.parser")
+  allLinks = soupObject.find_all('a')
+  return(allLinks)
+
+def extract(extractUrls):
+  articles = {}
+  for pageLinks in extractUrls:
+    if pageLinks:
+      if pageLinks.has_attr('href'):
+        articleTitle = pageLinks.text.strip().lower()
+        articleUrl = pageLinks['href']
+        articles[articleTitle] = articleUrl
+  return(articles)
+
+def relevance_check(articles, keywords):
+  rel = {}
+  for key in articles:
+    #relevant = False
+    for word in keywords:
+      if word in key and len(key.split()) > 3:
+        rel[key] = articles[key]
+        break
+
+      else:
+        continue
+  return(rel)
 
 def main():
   #Declare seeds
@@ -19,30 +52,10 @@ def main():
   linkQueue = queue.Queue()
   session = requests.session()
   for seed in seedUrls:
-    url = ''.join(seed)
-    req = urllib.request.Request(url)
-    req.add_header('User-Agent', 'Mozilla/5.0')
-    urlPage = urllib.request.urlopen(req)
-    charset = urlPage.info().get_content_charset()
-    pageContent = urlPage.read().decode(charset)
-    soupObject = bs(pageContent, "html.parser")
-    allLinks = soupObject.find_all('a')
-    for pageLinks in allLinks:
-      if pageLinks:
-        if pageLinks.has_attr('href'):
-          articleTitle = pageLinks.text.strip().lower()
-          articleUrl = pageLinks['href']
-          articles[articleTitle] = articleUrl
+    allLinks = crawl(seed)
+    articles = extract(allLinks)
+    relevantArticles = relevance_check(articles, keywords)
 
-    for key in articles:
-      relevant = False
-      for word in keywords:
-        if word in key and len(key.split()) > 3:
-          if key not in relevantArticles:
-            relevantArticles[key] = articles[key]
-          break
-        else:
-          continue
     for key in relevantArticles:
       print("{title} + {url}".format(title = key, url = relevantArticles[key]))
 
